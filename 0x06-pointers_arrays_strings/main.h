228e993a01d59e929245fa8e2c28a4f67f624dcb
geegosky <michaelokpala2004.com>
2022-12-29 02:23:11 -0800

    task 0

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,13 +1,10 @@
 #ifndef MAIN_H
-
 #define MAIN_H
-
 #include <stdio.h>
-
 /**
  * void prototypes()
- * int prototypes() *char prototypes()
- * void reverse.array(int a, int n);
+ * int prototypes()
+ * char prototypes()
  */
 void reverse_array(int *a, int n);
 void print_number(int n);
